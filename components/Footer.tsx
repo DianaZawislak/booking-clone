55afc33eb9964a6bfbc2bfd458ad19fb141f52eb
Diana Zawislak <diana.zawislak@icloud.com>
2024-01-02 18:41:56 -0500

    first build

---
+++ components/Footer.tsx
@@ -0,0 +1,168 @@
+"use client";
+
+import Link from "next/link";
+import { Fragment, useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+    ChatBubbleLeftIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    
+    PaperAirplaneIcon,
+    PhoneIcon,
+    PlayCircleIcon,
+    HomeModernIcon,
+    CameraIcon,
+} from "@heroicons/react/20/solid";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+import { cn } from "@/lib/utils";
+
+
+function Footer() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    return (
+        <header className="bg-[#1c2f6c] my-11">
+            <nav
+                className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-2"
+                aria-label="Global"
+            >
+                <div className="flex lg:flex-1">
+                    <Link href="/" className="">
+                        <span className="sr-only">Booking.com</span>
+                        <img
+                            className="h-12 w-auto"
+                            src="https://cdn.worldvectorlogo.com/logos/bookingcom-1.svg"
+                            alt=""
+                        />
+
+                    </Link>
+                </div>
+
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+                        onClick={() => setMobileMenuOpen(true)}
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                </div>
+
+                <Popover.Group className="hidden lg:flex lg:gap-x-9 mr-14">
+                    <div className="flex items-center space-x-2">
+                        <HomeModernIcon className="h-6 w-4 text-gray-400" aria-hidden="true" />
+                        <a href="#" className="text-sm font-semibold text-gray-400">
+                            Hotels
+                        </a>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <PaperAirplaneIcon className="h-6 w-4 text-gray-400" aria-hidden="true" />
+                        <a href="#" className="text-sm font-semibold text-gray-400">
+                            Flights
+                        </a>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <PaperAirplaneIcon className="h-6 w-4 text-gray-400" aria-hidden="true" />
+                        <a href="#" className="text-sm font-semibold text-gray-400">
+                            Car Rentals
+                        </a>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <CameraIcon className="h-6 w-4 text-gray-400" aria-hidden="true" />
+                        <a href="#" className="text-sm font-semibold text-gray-400">
+                            Attractions
+                        </a>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <PaperAirplaneIcon className="h-6 w-4 text-gray-400" aria-hidden="true" />
+                        <a href="#" className="text-sm font-semibold text-gray-400">
+                            Flight + Hotel
+                        </a>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <PhoneIcon className="h-6 w-4 text-gray-400" aria-hidden="true" />
+                        <a href="#" className="text-sm font-semibold text-gray-400">
+                            Customer Service
+                        </a>
+                    </div>
+                </Popover.Group>
+
+                <div className=" lg:flex lg:flex-1 lg:justify-end">
+                    <a href="#" className="text-lg font-semibold text-gray-400">
+                        © Diana Zawislak 2004
+                    </a>
+                </div>
+            </nav>
+
+            <Dialog
+                as="div"
+                className="lg:hidden"
+                open={mobileMenuOpen}
+                onClose={setMobileMenuOpen}
+            >
+                <div className="fixed inset-0 z-10" />
+
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#013B94] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Booking.com</span>
+                            <img
+                                className="h-8 w-auto"
+                                src="https://static1.squarespace.com/static/5bde0f00c3c16aa95581e2e2/62b4cb1add9d257dd43bb03d/62b653fedc7c895918d19b24/1656116254983/booking+logo+white.png?format=1500w"
+                                alt=""
+                            />
+                        </a>
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-2.5 text-white"
+                            onClick={() => setMobileMenuOpen(false)}
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800"
+                                >
+                                    Flights
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800"
+                                >
+                                    Car Rentals
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800"
+                                >
+                                    Attractions
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800"
+                                >
+                                    Flight + Hotel
+                                </a>
+                            </div>
+
+                            <div className="py-6">
+                            
+                            </div>
+                        </div>
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
+        </header>
+    );
+}
+
+export default Footer;
---
+++ data/trending.ts
@@ -0,0 +1,69 @@
+import { Flag } from "lucide-react";
+
+export const trending_data = [
+  {
+    id: 1,
+    src: "https://cf.bstatic.com/xdata/images/city/600x600/976784.jpg?k=717a6a83ea61edb06017bb8c9bd3d36511ec0e1aef59ac94235584d4fd1709cb&o=",
+    title: "New York City",
+    location: "USA",
+    flag: "https://www.countryflags.com/wp-content/uploads/united-states-of-america-flag-png-large.png",
+    description: "156 Deals",
+  },
+  {
+    id: 2,
+    src: "https://cf.bstatic.com/xdata/images/city/600x600/652869.jpg?k=ff0fcfe82f7b1bb9b5584027fbf8ff01af5aff6ce876035e5daebb407beb7371&o=",
+    title: "Gdynia",
+    location: "Poland",
+    flag: "https://www.countryflags.com/wp-content/uploads/poland-flag-png-large.png",
+    description: "56 deals",
+  },
+  {
+    id: 3,
+    src: "https://cf.bstatic.com/xdata/images/xphoto/540x405/289320924.webp?k=99a00f2907495aaeb6396695c053e3d8b95fb05619b10e76c89fb1f7d1fec427&o=",
+    title: "London",
+    location: "United Kingdom",
+    flag: "https://www.countryflags.com/wp-content/uploads/united-kingdom-flag-png-large.png",
+    description: "45 Deals",
+  },
+  {
+    id: 4,
+    src: "https://cf.bstatic.com/xdata/images/xphoto/540x405/290483794.webp?k=916f7bac0ccdb08efcb269ad29cc10816ab66cd1671359066d23d32fb17b5c39&o=",
+    title: "Singapore",
+    location: "Asia",
+    flag: "https://www.countryflags.com/wp-content/uploads/singapore-flag-png-large.png",
+    description: "88 Deals",
+  },
+  {
+    id: 5,
+    src: "https://cf.bstatic.com/xdata/images/city/600x600/977066.jpg?k=3659969918f1a86fc601a58056bd1a0f9c1b1e5205cc75e5dd83c5cc6c1ace49&o=",
+    title: "Las Vegas",
+    location: "United States of America",
+    flag: "https://www.countryflags.com/wp-content/uploads/united-states-of-america-flag-png-large.png",
+    description: "92 Deals",
+  },
+  {
+    id: 6,
+    src: "https://cf.bstatic.com/xdata/images/city/600x600/653082.jpg?k=c161c185c16c0402f72a69272e3757ffa3b45f5a28accb4c07a2c989625132cf&o=",
+    title: "Gdańsk",
+    location: "Poland",
+    flag: "https://www.countryflags.com/wp-content/uploads/poland-flag-png-large.png",
+    description: "92 Deals",
+  },
+  {
+    id: 7,
+    src: "https://cf.bstatic.com/xdata/images/city/600x600/976870.jpg?k=4bb231f86e973cac2971077b1664c1374fc0d718c95634b4cb01ae4550262f23&o=",
+    title: "Los Angeles",
+    location: "United States of America",
+    flag: "https://www.countryflags.com/wp-content/uploads/united-states-of-america-flag-png-large.png",
+    description: "92 Deals",
+  },
+  {
+    id: 8,
+    src: "https://r-xx.bstatic.com/xdata/images/city/170x136/682540.jpg?k=fc3655b3d7c5c4fd712aa84739e16d04fe7c68e7c1133fa4f6a7511d21fc50d7&o=",
+    title: "Sydney",
+    location: "Australia",
+    flag: "https://www.countryflags.com/wp-content/uploads/flag-jpg-xl-9-2048x1024.jpg",
+    description: "92 Deals",
+  },
+
+];
